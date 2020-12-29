05facfa7207f4d5719aab888e0e6cbce3a914f44
Rafal Pijet <stronglopez@wp.pl>
2020-12-29 18:06:49 +0100

    Add cart component with add items handling

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,22 +1,27 @@
 import React from "react";
 import pizzas from "../data/pizzas.json";
+import AppStateProvider from "./AppState";
 import Pizza from "./Pizza";
 import AppCss from "./App.module.css";
 import PizzaSVG from "../svg/pizza.svg";
+import Cart from "./Cart";
 
 const App = () => {
   return (
-    <div className={AppCss.container}>
-      <div className={AppCss.header}>
-        <PizzaSVG width={120} height={120} />
-        <div className={AppCss.siteTitle}>Delicious Pizza</div>
+    <AppStateProvider>
+      <div className={AppCss.container}>
+        <div className={AppCss.header}>
+          <PizzaSVG width={120} height={120} />
+          <div className={AppCss.siteTitle}>Delicious Pizza</div>
+          <Cart />
+        </div>
+        <ul>
+          {pizzas.map((pizza) => {
+            return <Pizza key={pizza.id} pizza={pizza} />;
+          })}
+        </ul>
       </div>
-      <ul>
-        {pizzas.map((pizza) => {
-          return <Pizza key={pizza.id} pizza={pizza} />;
-        })}
-      </ul>
-    </div>
+    </AppStateProvider>
   );
 };
 
--- src/components/Pizza.tsx
+++ src/components/Pizza.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import PizzaCss from './Pizza.module.css';
+import { useStateDispatch } from "./AppState";
+import PizzaCss from "./Pizza.module.css";
 
 interface Pizza {
   id: number;
@@ -13,11 +14,25 @@ interface Props {
 }
 
 const Pizza: React.FC<Props> = ({ pizza }) => {
+  const dispatch = useStateDispatch();
+  const handleAddToCartClick = () => {
+    dispatch({
+        type: 'ADD_TO_CART',
+        payload: {
+            item: {
+                id: pizza.id,
+                name: pizza.name,
+                price: pizza.price
+            }
+        }
+    })
+  };
   return (
     <li className={PizzaCss.container}>
       <h2>{pizza.name}</h2>
       <p>{pizza.description}</p>
       <p>{pizza.price}</p>
+      <button onClick={handleAddToCartClick}>Add to cart</button>
     </li>
   );
 };
